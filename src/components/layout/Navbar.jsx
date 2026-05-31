563132fd968a8e9f4b0f1ecf245330372f37ab8f
developer-aneeb <dev.aneeb.rehman@gmail.com>
2026-05-31 23:57:13 +0500

    fix the bugs

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Menu, X, Activity, ChevronRight, Sun, Moon } from 'lucide-react';
 import useTheme from '../../hooks/useTheme';
