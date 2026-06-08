885f0658b04ec4190b1a7f6dea62b99138f2a32b
developer-aneeb <dev.aneeb.rehman@gmail.com>
2026-06-09 01:17:10 +0500

    implemented the signin page

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import LandingPage from './pages/LandingPage';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
+function App() {
+    const [currentHash, setCurrentHash] = useState(window.location.hash || '#');
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            setCurrentHash(window.location.hash || '#');
+            window.scrollTo(0, 0);
+        };
+        window.addEventListener('hashchange', handleHashChange);
+        return () => window.removeEventListener('hashchange', handleHashChange);
+    }, []);
+
+    if (currentHash === '#signup') {
+        return <SignUp />;
+    }
+
+    if (currentHash === '#signin') {
+        return <SignIn />;
+    }
+
+    return <LandingPage />;
+}
+
+export default App;
